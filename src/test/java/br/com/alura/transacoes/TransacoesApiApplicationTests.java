b88fab2d8ad1fd9e9005d2b989b4cb3573e5ede6
Floriano Noguti <familianogutipvai@gmail.com>
2021-09-27 20:03:15 -0300

    Criação projeto Api REST carteira de transacoes com Spring Initializr

---
+++ src/test/java/br/com/alura/transacoes/TransacoesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
