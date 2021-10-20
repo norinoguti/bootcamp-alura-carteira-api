122829eacde4557fad6a5b4b4222c0a5ed0bb7c0
Floriano Noguti <familianogutipvai@gmail.com>
2021-10-20 17:17:24 -0300

    Documentação com SpringFox Swagger

---
+++ src/main/java/br/com/alura/transacoes/infra/SpringFoxSwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package br.com.alura.transacoes.infra;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxSwaggerConfiguration {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	      "API Carteira de Investimentos", 
+	      "API de controle carteira de investimentos.", 
+	      "Termos de uso", 
+	      "Termos de Serviço", 
+	      new Contact("Floriano Noguti", "www.example.com", "email@company.com"), 
+	      "License of API", "API license URL", Collections.emptyList());
+	}
+}
