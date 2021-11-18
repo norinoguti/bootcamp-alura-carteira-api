7fabe94fde6d0bf545923b0e5efe760b394d0d30
Floriano Noguti <familianogutipvai@gmail.com>
2021-11-18 01:54:01 -0300

    Correções arquivos application

--- src/main/java/br/com/alura/transacoes/controller/HomeController.java
+++ src/main/java/br/com/alura/transacoes/controller/HomeController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
 @RestController
 @RequestMapping("/home")
+@Api(tags = "Home")
 public class HomeController {
 	
 	@GetMapping
